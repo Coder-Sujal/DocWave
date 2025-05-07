5b0886dd13d979abd3ce2a095f0f380b4fc580e0
Coder-Sujal <sujalsmit20@gmail.com>
2025-05-07 19:15:41 +0530

    Open document and delete document functionality added

---
+++ docs-app/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://deep-hen-25.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- docs-app/src/app/(home)/template-gallery.tsx
+++ docs-app/src/app/(home)/template-gallery.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
 import {
   Carousel,
   CarouselContent,
@@ -7,11 +11,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+
 import { cn } from "@/lib/utils";
 import { templates } from "@/constants/templates";
+import { api } from "../../../convex/_generated/api";
 
 export const TemplateGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
 
   return (
     <div className="bg-[#F1F3F4]">
@@ -32,7 +51,7 @@ export const TemplateGallery = () => {
                 >
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    onClick={() => onTemplateClick(template.label,"")}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
