3db826d28ef4818ccbc33dd2a148e9d0c3d77405
Coder-Sujal <sujalsmit20@gmail.com>
2025-05-23 18:41:18 +0530

    Rename functionality added inside document

--- docs-app/src/app/documents/[documentId]/navbar.tsx
+++ docs-app/src/app/documents/[documentId]/navbar.tsx
@@ -42,8 +42,14 @@ import { Avatars } from "./avatar";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Inbox } from "./inbox";
+import { Doc } from "../../../../convex/_generated/dataModel";
 
-export const Navbar = () => {
+interface NavbarProps { 
+  data : Doc<"documents">;
+}
+
+
+export const Navbar = ({data} : NavbarProps) => {
   const { editor } = useEditorStore();
 
   const InsertTable = ({ rows, cols }: { rows: number; cols: number }) => {
@@ -70,7 +76,7 @@ export const Navbar = () => {
       type: "application/json",
     });
 
-    onDownload(blob, `document.json`);
+    onDownload(blob, `${data.title}.json`);
   };
 
   const onSaveHTML = () => {
@@ -81,7 +87,7 @@ export const Navbar = () => {
       type: "text/html",
     });
 
-    onDownload(blob, `document.html`);
+    onDownload(blob, `${data.title}.html`);
   };
 
   const onSaveText = () => {
@@ -92,7 +98,7 @@ export const Navbar = () => {
       type: "text/plain",
     });
 
-    onDownload(blob, `document.txt`);
+    onDownload(blob, `${data.title}.txt`);
   };
 
   return (
@@ -102,7 +108,7 @@ export const Navbar = () => {
           <Image src="/logo.svg" alt="DockWave Logo" width={36} height={36} />
         </Link>
         <div className="flex flex-col">
-          <DocumentInput />
+          <DocumentInput title = {data.title} id = {data._id}/>
           <div className="flex">
             <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
               <MenubarMenu>
